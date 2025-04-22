2372c87e3e6e0d3a6e82e8e8654e19f0b5767377
jailennn <111888334+jailennn@users.noreply.github.com>
2025-04-21 20:56:42 -0400

    Update refresh.sh

--- deployment/refresh.sh
+++ deployment/refresh.sh
@@ -3,13 +3,13 @@
 # did this by combining commands from https://docs.docker.com/reference/cli/docker/ and adding my specific configuration.
 
 echo "-- stopping and removing existing container --"
-docker kill angular-ct
-docker rm angular-ct ## removing it so that there arent name errors
+docker kill angular-app
+docker rm angular-app ## removing it so that there arent name errors
 
 echo "-- pulling latest image from DockerHub --"
 docker pull wsujduncan/angular-bird:latest
 
 echo "-- starting new container image --"
-docker run -it --name angular-ct -p 8080:4200 wsujduncan/angular-bird:latest
+docker run -it --name angular-app -p 8080:4200 wsujduncan/angular-bird:latest
 
 echo "-- done. New container version running --"
